6cba453a5202832a534c5e7521a160967d3aeba4
Sanket Naik <sanketnaik99@gmail.com>
2022-10-26 00:06:22 -0700

    Updates skeleton script

--- scripts/skeleton-gpu-batch-script.sh
+++ scripts/skeleton-gpu-batch-script.sh
@@ -15,15 +15,19 @@ export LIBOMPTARGET_INFO=4
 # it is assumed your environment is set up properly for using the Cori GPUs
 # prior to you launching this batch script
 #
-for N in 32 64 128 256 512 1024 # loop over problem sizes
-# for N in 32 # loop over problem sizes
+for N in 32 64 128 256 512 1024 # loop over number of threads per block
+# for N in 32 # loop over number of threads per block
     do
-    for B in 1 4 16 64 256 1024 4096
-    # for B in 1 4
+    for B in 1 4 16 64 256 1024 4096 # loop over block sizes
+    # for B in 1 4 # loop over block sizes
         do
-        echo -e "\n\nStarted new run\n"
-        echo nvprof -s -m sm_efficiency ./sobel_gpu -N $N -B $B
-        nvprof -s -m sm_efficiency ./sobel_gpu -N $N -B $B
+        echo -e "\n\nStarted new run for $N threads per block and $B block size\n"
+        echo -e "\nRUNTIME METRIC\n"
+        echo nvprof ./sobel_gpu -N $N -B $B
+        nvprof ./sobel_gpu -N $N -B $B
+        echo -e "\nSM EFFICIENCY METRIC\n"
+        echo nvprof -m sm_efficiency ./sobel_gpu -N $N -B $B
+        nvprof -m sm_efficiency ./sobel_gpu -N $N -B $B
     done
 done
 echo nvprof -m sm_efficiency ./sobel_cpu_omp_offload
